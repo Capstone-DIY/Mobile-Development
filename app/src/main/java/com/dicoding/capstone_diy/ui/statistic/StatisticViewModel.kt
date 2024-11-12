b53b89eb9519f0ed8d52ea6d5dc035de7783d770
keesmoo <bisma2412@gmail.com>
2024-11-13 00:35:47 +0800

    Home

--- app/src/main/java/com/dicoding/capstone_diy/ui/statistic/StatisticViewModel.kt
+++ app/src/main/java/com/dicoding/capstone_diy/ui/statistic/StatisticViewModel.kt
@@ -1,10 +1,10 @@
-package com.dicoding.capstone_diy.ui.dashboard
+package com.dicoding.capstone_diy.ui.statistic
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class StatisticViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
